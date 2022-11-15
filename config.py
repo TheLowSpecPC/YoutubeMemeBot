faf3b2eb6b7c82bce526d54f3842553cf3f65786
Alen Roy <80385481+TheLowSpecPC@users.noreply.github.com>
2022-11-15 12:07:24 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+#Reddit Looter
+client_id=""
+client_secret=""
+user_agent=""
+username=""
+
+limit = 20
+subreddit = 'videomemes','dankvideos','shitposting','funny'
+
+#Youtube Bot
+gmail = ""
+password = ""
+upload_button = ""
+
+#Output Video Name
+intro_video = "Example.mp4"
+transition_video = "Example.mp4"
+outro_video = "Example.mp4"
+name = "Example.mp4"
